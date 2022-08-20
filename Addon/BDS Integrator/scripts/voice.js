d64603d8e1e796f887bfe8ec877e9041711ac3e6
MADLAD3718 <scheuringjack@gmail.com>
2022-08-20 17:04:31 -0400

    Finalize group format

--- Addon/BDS Integrator/scripts/voice.js
+++ Addon/BDS Integrator/scripts/voice.js
@@ -7,12 +7,12 @@ const groupedPlayers = new Set();
 
 export function voice() {
     world.events.tick.subscribe(() => {
-        // Update Groups
+        // Update Groups:
         groups.forEach(group => {
             group.update();
         })
 
-        // Groups
+        // Ungrouped Players:
         const query = new EntityQueryOptions();
         query.tags = [`linked`];
         // For all players that have linked their accounts and aren't in a group
@@ -33,7 +33,7 @@ export function voice() {
                     groupedPlayers.add(player.name)
                     groupedPlayers.add(otherPlayer.name);
                     groups.add(
-                        new Group(player, otherPlayer)
+                        new Group([player, otherPlayer])
                     )
                     world.getDimension('overworld').runCommand(`say Grouped ${player.name} & ${otherPlayer.name}`);
                     exportGroups();
@@ -47,13 +47,44 @@ export function voice() {
                 const zDist = Math.abs(group.center.z - player.location.z);
 
                 const hDist = Math.sqrt(Math.pow(xDist, 2) + Math.pow(zDist, 2));
-                const inRange = hDist <= variables.get("horizontal-range") && yDist <= variables.get("vertical-range") && player.dimension.id === group.dimension ? true : false;
-                const groupPlayers = [...group.players];
+                const inRange = hDist <= group.range.h && yDist <= group.range.v && player.dimension.id === group.dimension ? true : false;
                 if (inRange === true) {
                     group.addPlayer(player.name);
                 }
             })
         }
+
+        // Group Merging
+        groups.forEach(group => {
+            groups.forEach(otherGroup => {
+                if (otherGroup.center === group.center) return;
+                const xDist = Math.abs(otherGroup.center.x - group.center.x);
+                const yDist = Math.abs(otherGroup.center.y - group.center.y);
+                const zDist = Math.abs(otherGroup.center.z - group.center.z);
+                const hDist = Math.sqrt(Math.pow(xDist, 2) + Math.pow(zDist, 2));
+                if ((hDist <= group.range.h || hDist <= otherGroup.range.h) && (yDist <= group.range.v || yDist <= otherGroup.range.v)) {
+                    world.getDimension('overworld').runCommand(`say Group ${[...group.players]} is in range of ${[...otherGroup.players]}`);
+                    const newPlayers = [];
+                    [...group.players, ...otherGroup.players].forEach(playerName => {
+                        world.getDimension('overworld').runCommand(`say Group has: ${playerName}`);
+                        const query = new EntityQueryOptions();
+                        query.name = playerName;
+                        for (const player of world.getPlayers(query)) {
+                            newPlayers.push(player);
+                        }
+                    })
+                    groups.add(
+                        new Group(newPlayers)
+                    );
+                    groups.delete(group);
+                    groups.delete(otherGroup);
+                    groups.forEach(group => {
+                        world.getDimension('overworld').runCommand(`say The ONE GROUP is ${[...group.players]}`);
+                    })
+                    return;
+                }
+            })
+        })
     })
     world.events.playerLeave.subscribe(event => {
         groups.forEach(group => {
@@ -63,9 +94,11 @@ export function voice() {
 }
 
 function exportGroups() {
-    const groupExport = [];
+    const groupExport = {};
     groups.forEach(group => {
-        if (group.players.size > 1) groupExport.push([...group.players]);
+        if (group.players.size > 1) groupExport[group.id] = {
+            members: [...group.players]
+        }
         // world.getDimension('overworld').runCommand(`say Added ${groupExport} to groupExport[]`);
     })
     // world.getDimension('overworld').runCommand(`say Sent ${JSON.stringify(groupExport)}`);
@@ -84,15 +117,24 @@ function exportGroups() {
 }
 
 class Group {
+    range = {
+        h: variables.get("horzontal-range") / 2,
+        v: variables.get("vertical-range")
+    }
     center = new Location(0, 0, 0);
     dimension = '';
     players = new Set();
-    constructor(...args) {
-        args.forEach((player, i) => {
-            this.players.add(player.name);
-            if (i === 0) this.dimension = player.dimension.id;
-        })
-        this.update();
+    constructor(playerArray) {
+        try {
+            playerArray.forEach((player, i) => {
+                this.players.add(player.name);
+                if (i === 0) this.dimension = player.dimension.id;
+            })
+            this.id = Math.round(Math.random() * 99999999).toString().padStart(8, '0');
+            this.update();            
+        } catch (error) {
+            world.getDimension('overworld').runCommand(`say ${error}${error.stack}`);
+        }
     }
     addPlayer(playername) {
         // world.getDimension('overworld').runCommand(`say Added ${playername} to group.`)
@@ -117,9 +159,11 @@ class Group {
 
         const hDist = Math.sqrt(Math.pow(xDist, 2) + Math.pow(zDist, 2));
         // world.getDimension('overworld').runCommand(`say ${player.name} hDist: ${hDist}.`)
-        const outsideRange = hDist <= (variables.get("horizontal-range") / 2 + variables.get("leave-threshold")) && yDist <= (variables.get("vertical-range") + variables.get("leave-threshold") / 2) && player.dimension.id === this.dimension ? false : true;
+        // eventually increase this by amount of players
+        const outsideRange = hDist <= this.range.h && yDist <= this.range.v && player.dimension.id === this.dimension ? false : true;
         if (outsideRange === true) {
             this.removePlayer(player.name);
+            this.getRange();
         }
         return outsideRange;
     }
@@ -142,8 +186,13 @@ class Group {
         // world.getDimension('overworld').runCommand(`say Recalculated center to be at (${Math.round(x)}, ${Math.round(y)}, ${Math.round(z)})`);
         this.center = new Location(x, y, z);
     }
+    getRange() {
+        this.range.h = (variables.get("horizontal-range") + (this.players.size - 1) * variables.get("player-addition")) / 2;
+        this.range.v = (variables.get("vertical-range") + (this.players.size - 1) * variables.get("player-addition") / 2) / 2;
+    }
     update() {
         this.getCenter();
+        this.getRange();
         this.players.forEach(playerName => {
             const query = new EntityQueryOptions();
             query.name = playerName;
