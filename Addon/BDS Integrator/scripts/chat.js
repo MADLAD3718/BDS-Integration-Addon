ebc9520fc91128f22ece2b0b26a63da1db6263bf
bigguy69420 <jaylyextra@gmail.com>
2022-08-23 10:25:20 +0800

    format

--- Addon/BDS Integrator/scripts/chat.js
+++ Addon/BDS Integrator/scripts/chat.js
@@ -7,15 +7,15 @@ import { http, HttpRequest, HttpRequestMethod } from "mojang-net";
  * @param {ChatEvent} event 
  */
 export function chat(event) {
-        const request = new HttpRequest(`https://bdsintegrator.ddns.net/api`);
-        request.addHeader("Content-Type", "application/json")
-        request.addHeader("mc-data-type", "chat-message")
-        request.addHeader("server-uuid", variables.get('server-uuid'))
-        request.body = JSON.stringify({
-            username: event.sender.name,
-            message: event.message
-        })
-        request.method = HttpRequestMethod.POST;
+    const request = new HttpRequest(`https://bdsintegrator.ddns.net/api`);
+    request.addHeader("Content-Type", "application/json")
+    request.addHeader("mc-data-type", "chat-message")
+    request.addHeader("server-uuid", variables.get('server-uuid'))
+    request.body = JSON.stringify({
+        username: event.sender.name,
+        message: event.message
+    })
+    request.method = HttpRequestMethod.POST;
 
-        http.request(request);
+    http.request(request);
 }
